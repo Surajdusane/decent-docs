360f85be7b739f1684c9e2e26a8af08769d5822f
Surajdusane <138127406+Surajdusane@users.noreply.github.com>
2025-04-19 00:33:30 +0530

    og

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -80,6 +80,12 @@ export const viewport: Viewport = {
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <html lang="en" className={inter.className} suppressHydrationWarning>
+      <head>
+  <meta
+    property="og:image"
+    content="https://decent-docs.vercel.app/api/static"
+  />
+</head>
       <body className="flex flex-col min-h-screen">
         <RootProvider theme={{
           defaultTheme: "system"
