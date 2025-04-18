ee4f48c1cde33175fe4be379c7db7eefffbdd4f6
Surajdusane <138127406+Surajdusane@users.noreply.github.com>
2025-04-18 23:33:08 +0530

    google analytics

--- src/components/global/analytics.tsx
+++ src/components/global/analytics.tsx
@@ -1,9 +1,23 @@
-import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script';
 
 export default function Analytics() {
-  return (
-    <>
-        <GoogleAnalytics gaId={"G-84L4LLL6ZJ"} />
-    </>
-  )
+    return (
+        <>
+          <Script
+            strategy='lazyOnload'
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}`}
+          />
+    
+          <Script id='' strategy='lazyOnload'>
+            {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID}', {
+                  page_path: window.location.pathname,
+                  });
+              `}
+          </Script>
+        </>
+      );
 }
