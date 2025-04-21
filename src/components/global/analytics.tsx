18dae75e9aefde36176bc1ace109d3766de721de
Surajdusane <138127406+Surajdusane@users.noreply.github.com>
2025-04-22 00:28:31 +0530

    integret vercel analytics

--- src/components/global/analytics.tsx
+++ src/components/global/analytics.tsx
@@ -1,20 +1,9 @@
-import Script from 'next/script';
+import { Analytics as AnalyticsComponent } from '@vercel/analytics/next';
 
 export default function Analytics() {
     return (
         <>
-          <Script
-            async src={`https://www.googletagmanager.com/gtag/js?id=G-84L4LLL6ZJ`}
-          />
-    
-          <Script id='' strategy='lazyOnload'>
-            {`
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', 'G-84L4LLL6ZJ')
-              `}
-          </Script>
+          <AnalyticsComponent />
         </>
       );
 }
